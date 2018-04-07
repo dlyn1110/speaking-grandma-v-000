065faed2e07a3bbd640bd081bcc719d6d5e30aa0
Dani Lyn <dlyn1110@gmail.com>
2018-04-07 21:03:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma
-  if 
+  if
   elsif condition
   else
 end    
+end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!

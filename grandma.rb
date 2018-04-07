65f39ea267cb0f57ede6dc3e0e8bb6d4331c8c1f
Dani Lyn <dlyn1110@gmail.com>
2018-04-07 21:51:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
+    return "NO, NOT SINCE 1938!"
+  else
+    return "I LOVE YOU TOO PUMPKIN!"
  end
 end
 
